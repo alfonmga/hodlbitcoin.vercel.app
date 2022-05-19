0d7909b10a51143d0cc2e71b31506f61868a7679
alfonmga <alfonmga@icloud.com>
2022-05-20 01:46:52 +0200

    Fix: window.initSqlJs pre-check

--- public/sql-loader.js
+++ public/sql-loader.js
@@ -4,8 +4,8 @@ s.setAttribute("src", "/sql-wasm.js");
 document.body.appendChild(s);
 
 window.loadSQL = async () => {
+  if (typeof window.initSqlJs === "undefined") return;
   console.log("loadSQL function called");
-
   return await initSqlJs({
     locateFile: (file) => `/${file}`,
   });
