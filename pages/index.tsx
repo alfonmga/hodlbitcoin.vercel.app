46d08e8f1e519b727c7620eb721b9fff4a660997
alfonmga <alfonmga@icloud.com>
2022-05-20 16:09:47 +0200

    Add generate chart button, improve code, bump to v0.0.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,25 @@ import {
   Chart as ChartJS,
   Legend,
   LinearScale,
+  LineController,
   LineElement,
   LogarithmicScale,
   PointElement,
   TimeScale,
   Title,
   Tooltip,
-  LineController,
 } from "chart.js";
 import "chartjs-adapter-date-fns";
-import fromUnixTime from "date-fns/fromUnixTime";
 import formatDate from "date-fns/format";
+import fromUnixTime from "date-fns/fromUnixTime";
 import { enUS } from "date-fns/locale";
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import { useMemo, useState } from "react";
+import { Dispatch, SetStateAction, useMemo, useState } from "react";
 import { Chart } from "react-chartjs-2";
+import { QueryExecResult } from "sql.js";
 import { useDB, useDBQuery } from "../lib/useDb";
+
 ChartJS.register(
   LogarithmicScale,
   LineController,
@@ -33,7 +35,7 @@ ChartJS.register(
   Title
 );
 
-const DEFAULT_HOLDINGS_VALUE = 1;
+const DEFAULT_HOLDINGS_AMOUNT_VALUE = 1;
 
 const Home: NextPage<{ dbBinStr: string }> = ({ dbBinStr }) => {
   const dbBin = useMemo(
@@ -42,52 +44,17 @@ const Home: NextPage<{ dbBinStr: string }> = ({ dbBinStr }) => {
   );
   const db = useDB(dbBin);
   const [query, _] = useState("SELECT date, price FROM prices;");
-  const results = useDBQuery(db, query);
-  const [holdingsInputV, setHoldingsInputV] = useState<string>(
-    String(DEFAULT_HOLDINGS_VALUE)
+  const bitcoinPricesData = useDBQuery(db, query);
+  const [holdingsAmountV, setHoldingsAmountV] = useState<number>(
+    DEFAULT_HOLDINGS_AMOUNT_VALUE
   );
-  const [holdingsV, setHoldingsV] = useState<number>(DEFAULT_HOLDINGS_VALUE);
-
-  const chartData = useMemo(() => {
-    let labels: any[] = [];
-    let data: any[] = [];
-    if (results !== null) {
-      results[0].values.map((v) => {
-        const d = fromUnixTime(v[0] as number);
-        const p = v[1] as number;
-        labels.push(d);
-        data.push({ x: d, y: p, holdingsV: p * holdingsV });
-      });
-    }
-
-    return {
-      labels,
-      datasets: [
-        {
-          label: "Price",
-          data,
-          borderColor: "#f2a900",
-          backgroundColor: "#f2a900",
-          pointRadius: 0,
-        },
-      ],
-    };
-  }, [results, holdingsV]);
-
-  const onHoldingsValueInputChange = (value: string) => {
-    setHoldingsInputV(value);
-    let n = Number(value);
-    if (Number.isNaN(n) === false) {
-      setHoldingsV(Number(n.toFixed(8)));
-    }
-  };
 
   return (
     <>
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+          content="width=1480, minimal-ui, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
       </Head>
       <div>
@@ -110,97 +77,170 @@ const Home: NextPage<{ dbBinStr: string }> = ({ dbBinStr }) => {
           <h1 style={{ color: "white" }}>
             Visualize your Bitcoin holdings value over time
           </h1>
-          <input
-            style={{ marginTop: "-22px" }}
-            type="text"
-            value={holdingsInputV}
-            onChange={(evt) =>
-              onHoldingsValueInputChange(evt.currentTarget.value)
-            }
-            placeholder="0.00000000 BTC"
+          <HoldingsInputField
+            currentHoldingsAmountV={holdingsAmountV}
+            onChange={(v) => setHoldingsAmountV(v)}
           />
         </div>
-
-        <div
-          style={{
-            display: "flex",
-            alignContent: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div style={{ width: "95%" }}>
-            <Chart
-              // key={generatedHoldingsV}
-              data={chartData}
-              options={{
-                interaction: {
-                  mode: "nearest",
-                  axis: "x",
-                  intersect: false,
-                },
-                responsive: true,
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                  tooltip: {
-                    backgroundColor: "#222531",
-                    callbacks: {
-                      title: function (j) {
-                        return formatDate(j[0].parsed.x, "MM/dd/yyyy");
-                      },
-                      label: function (context) {
-                        return [
-                          `${context.dataset.label}: ${new Intl.NumberFormat(
-                            "en-US",
-                            {
-                              style: "currency",
-                              currency: "USD",
-                            }
-                          ).format(context.parsed.y)}`,
-                          `Holdings value ${new Intl.NumberFormat("en-US", {
-                            style: "currency",
-                            currency: "USD",
-                          }).format((context.raw as any).holdingsV as number)}`,
-                        ];
-                      },
-                    },
-                  },
-                },
-                scales: {
-                  x: {
-                    grid: {
-                      color: "#222531",
-                    },
-                    display: true,
-                    type: "time",
-                    time: {
-                      unit: "year",
-                    },
-                    adapters: {
-                      date: {
-                        locale: enUS,
-                      },
-                    },
-                  },
-                  y: {
-                    grid: {
-                      color: "#222531",
-                    },
-                    display: true,
-                    type: "logarithmic",
-                    ticks: {
-                      color: "#858ca2",
-                    },
-                  },
-                },
-              }}
-              type="line"
-            />
-          </div>
-        </div>
+        <HoldingsChart
+          key={holdingsAmountV}
+          bitcoinPricesData={bitcoinPricesData}
+          holdingsAmountV={holdingsAmountV}
+        />
       </div>
     </>
+  );
+};
+const HoldingsInputField = ({
+  currentHoldingsAmountV,
+  onChange,
+}: {
+  currentHoldingsAmountV: number;
+  onChange: (value: number) => void;
+}) => {
+  const [holdingsInputV, setHoldingsInputV] = useState<string>(
+    String(currentHoldingsAmountV)
+  );
+  const [generatedV, setGeneratedV] = useState(currentHoldingsAmountV);
+  const onGenerate = () => {
+    let n = Number(holdingsInputV);
+    if (Number.isNaN(n) === false) {
+      setGeneratedV(generatedV);
+      onChange(Number(n.toFixed(8)));
+    }
+  };
+
+  return (
+    <>
+      <input
+        style={{ marginTop: "-22px" }}
+        type="text"
+        value={holdingsInputV}
+        onChange={(evt) => setHoldingsInputV(evt.currentTarget.value)}
+        placeholder="0.00000000 BTC"
+      />
+      <button
+        onClick={onGenerate}
+        disabled={
+          holdingsInputV.length === 0 || holdingsInputV === String(generatedV)
+        }
+      >
+        Generate chart
+      </button>
+    </>
+  );
+};
+const HoldingsChart = ({
+  bitcoinPricesData,
+  holdingsAmountV,
+}: {
+  bitcoinPricesData: QueryExecResult[] | null;
+  holdingsAmountV: number;
+}) => {
+  const chartData = useMemo(() => {
+    let labels: any[] = [];
+    let data: any[] = [];
+    if (bitcoinPricesData !== null) {
+      bitcoinPricesData[0].values.map((v) => {
+        const d = fromUnixTime(v[0] as number);
+        const p = v[1] as number;
+        labels.push(d);
+        data.push({ x: d, y: holdingsAmountV * p, price: p });
+      });
+    }
+
+    return {
+      labels,
+      datasets: [
+        {
+          label: "Holdings value",
+          data,
+          borderColor: "#f2a900",
+          backgroundColor: "#f2a900",
+          pointRadius: 0,
+        },
+      ],
+    };
+  }, [bitcoinPricesData, holdingsAmountV]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ width: "95%" }}>
+        <Chart
+          data={chartData}
+          options={{
+            interaction: {
+              mode: "nearest",
+              axis: "x",
+              intersect: false,
+            },
+            responsive: true,
+            plugins: {
+              legend: {
+                display: false,
+              },
+              tooltip: {
+                backgroundColor: "#222531",
+                callbacks: {
+                  title: function (j) {
+                    return formatDate(j[0].parsed.x, "MM/dd/yyyy");
+                  },
+                  label: function (context) {
+                    return [
+                      `${context.dataset.label}: ${new Intl.NumberFormat(
+                        "en-US",
+                        {
+                          style: "currency",
+                          currency: "USD",
+                        }
+                      ).format(context.parsed.y)}`,
+                      `Bitcoin price: ${new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                      }).format((context.raw as any).price as number)}`,
+                    ];
+                  },
+                },
+              },
+            },
+            scales: {
+              x: {
+                grid: {
+                  color: "#222531",
+                },
+                display: true,
+                type: "time",
+                time: {
+                  unit: "year",
+                },
+                adapters: {
+                  date: {
+                    locale: enUS,
+                  },
+                },
+              },
+              y: {
+                grid: {
+                  color: "#222531",
+                },
+                display: true,
+                type: "logarithmic",
+                ticks: {
+                  color: "#858ca2",
+                },
+              },
+            },
+          }}
+          type="line"
+        />
+      </div>
+    </div>
   );
 };
 
